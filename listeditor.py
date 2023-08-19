25899f4245f62836d0f85ae48d0fa955de0b2e34
Ivan <101908819+navirobayo@users.noreply.github.com>
2023-08-18 23:29:14 -0500

    Delete listeditor.py

--- listeditor.py
+++
@@ -1,8 +0,0 @@
-sombrero = [1, 2, 3, 4, 5]
-print(sombrero)
-print("Bienvenido al encriptador de listas.")
-digito_magico = int(input("Ingresa una cifra viajero del tiempo."))
-sombrero[2]=digito_magico
-del(sombrero[-1])
-print(sombrero)
-print(len(sombrero))
